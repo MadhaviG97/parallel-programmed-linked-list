b81c1c1ae7e27b9b9ffb54ac26cc2be3c366d54c
Amanda Ariyaratne <aari@pop-os.localdomain>
2021-07-24 09:02:28 +0530

    extract new function
    Extract the task of generating m number of operations to a separate function. Define global constants.

--- main.c
+++ main.c
@@ -3,80 +3,96 @@
 #include<time.h>
 #include<math.h>
 
+#define upper (int) pow((double) 2, 16) - 1
+#define lower 0 
+#define n 10
+#define m 10000
+#define op_upper 1000
+#define op_lower 1
+#define p_member 0.99
+#define p_insert 0.005
+#define p_delete 0.005
+
 struct list_node_s {
     int data;
     struct list_node_s* next;
 };
 
+typedef int (*ll_operation)(int, struct list_node_s**);
+
+void generateRandomOperations(ll_operation operation_arr[], int operand_arr[]);
 int Insert(int value, struct list_node_s** head_pp);
 int Member(int value, struct list_node_s** head_pp);
 int Delete(int value, struct list_node_s** head_pp);
 
 int main(){
-    int upper= (int) pow((double) 2, 16) - 1;
-    int lower=0;
-    int n=5; int m=10000;   
-    int op_upper=1000; int op_lower=1; 
 
-    int m_member = (int) (0.99*m); 
-    int m_insert = (int) (0.005*m); 
-    int m_delete = (int) (0.005*m);
+    ll_operation operation_arr[m] = { NULL };
+    int operand_arr[m];
 
-    const int w_member = (int) (0.99*op_upper); 
-    const int w_insert = (int) (0.005*op_upper); 
-    const int w_delete = (int) (0.005*op_upper);
+    generateRandomOperations(operation_arr, operand_arr);
     
-    struct list_node_s* ll_head=NULL;
+    struct list_node_s* ll_head = NULL;
 
     srand(time(0));
     
-    int i=0;
+    int i = 0;
     while (i < n){            
         int rand_numb = (rand() % (upper - lower + 1)) + lower;
         int return_val = Insert(rand_numb, &ll_head);
-        if (return_val==1){
+        if (return_val == 1){
             i++;
         }
     }
     
-    int (*operation_arr[m])(int, struct list_node_s**);
-    int operand_arr[m];
-    i=0;
-    while ((m_member+m_insert+m_delete)!=0)
-    {
-        int operation = (rand() % (op_upper-op_lower+1)) + op_lower;
-        int rand_numb = (rand() % (upper - lower)) + lower;
-
-        if ((w_member >= operation) && (m_member!=0)){
-            m_member--;
-            operation_arr[i]=Member;
-            operand_arr[i]=rand_numb;
-            i++;
-        }
-        else if ((w_member < operation) && 
-                    (w_insert+w_member >= operation) && 
-                    (m_insert!=0)){
-            m_insert--;
-            operation_arr[i]=Insert;
-            operand_arr[i]=rand_numb;
-            i++;
-        }
-        else if (((w_insert+w_member) < operation) && 
-                    (w_delete+w_insert+w_member >= operation) && 
-                    (m_delete!=0)){
-            m_delete--;
-            operation_arr[i]=Delete;
-            operand_arr[i]=rand_numb;
-            i++;
-        }
-    }
-
     for (i = 0; i < m; i++)
     {
         (*operation_arr[i])(operand_arr[i], &ll_head);
     }    
 
     return 0;
+}
+
+void generateRandomOperations(ll_operation operation_arr[], int operand_arr[]) {
+
+    int m_member = (int) (p_member*m); 
+    int m_insert = (int) (p_insert*m); 
+    int m_delete = (int) (p_delete*m);
+
+    const int w_member = (int) (p_member*op_upper); 
+    const int w_insert = (int) (p_insert*op_upper); 
+    const int w_delete = (int) (p_delete*op_upper);
+    
+    int i = 0;
+    while ((m_member + m_insert + m_delete) != 0)
+    {
+        int operation = (rand() % (op_upper - op_lower + 1)) + op_lower;
+        int rand_numb = (rand() % (upper - lower)) + lower;
+
+        if ((w_member >= operation) && (m_member != 0)){
+            m_member--;
+            operation_arr[i] = Member;
+            operand_arr[i] = rand_numb;
+            i++;
+        }
+        else if ((w_member < operation) && 
+                    (w_insert + w_member >= operation) && 
+                    (m_insert != 0)){
+            m_insert--;
+            operation_arr[i] = Insert;
+            operand_arr[i] = rand_numb;
+            i++;
+        }
+        else if (((w_insert + w_member) < operation) && 
+                    (w_delete + w_insert + w_member >= operation) && 
+                    (m_delete != 0)){
+            m_delete--;
+            operation_arr[i] = Delete;
+            operand_arr[i] = rand_numb;
+            i++;
+        }
+    }
+    return;
 }
 
 int Insert(int value, struct list_node_s** head_pp){
@@ -108,7 +124,7 @@ int Member(int value, struct list_node_s** head_pp){
     struct list_node_s* curr_p = *head_pp;
 
     while (curr_p !=NULL && curr_p->data < value){ 
-        // printf("item: %d\n", curr_p->data);
+        printf("item: %d\n", curr_p->data);
         curr_p = curr_p->next;
     }
 
